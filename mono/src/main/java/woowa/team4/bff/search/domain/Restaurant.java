a14a06a94a978ac64922693ddc23e5b72df4ea5b
huiseung <whtjddn75@gmail.com>
2024-08-09 21:51:45 +0900

    [fix] 생성자, toString 추가

--- mono/src/main/java/woowa/team4/bff/search/domain/Restaurant.java
+++ mono/src/main/java/woowa/team4/bff/search/domain/Restaurant.java
@@ -1,31 +1,27 @@
 package woowa.team4.bff.search.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 
+@Getter
+@Builder
+@AllArgsConstructor
 public class Restaurant {
     private String id;
     private String restaurantName;
     private String restaurantId;
 
-    @Builder
-    public Restaurant(String restaurantName, String restaurantId) {
-        this.restaurantName = restaurantName;
-        this.restaurantId = restaurantId;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getRestaurantName() {
-        return restaurantName;
-    }
-
-    public String getRestaurantId() {
-        return restaurantId;
-    }
-
     public void update(String restaurantName){
         this.restaurantName = restaurantName;
+    }
+
+    @Override
+    public String toString() {
+        return "Restaurant{" +
+                "id='" + id + '\'' +
+                ", restaurantName='" + restaurantName + '\'' +
+                ", restaurantId='" + restaurantId + '\'' +
+                '}';
     }
 }
