2cdb38218a95b015f53b9ca355a2b32cc7b1b88c
huiseung <whtjddn75@gmail.com>
2024-08-09 21:51:45 +0900

    [fix] 생성자, toString 추가

--- mono/src/main/java/woowa/team4/bff/search/document/RestaurantDocument.java
+++ mono/src/main/java/woowa/team4/bff/search/document/RestaurantDocument.java
@@ -1,11 +1,15 @@
 package woowa.team4.bff.search.document;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
 
+@Getter
 @Document(indexName = "restaurants")
+@AllArgsConstructor
 public class RestaurantDocument {
     @Id
     private String id;
@@ -15,4 +19,13 @@ public class RestaurantDocument {
 
     @Field(type = FieldType.Keyword, name = "restaurant_id")
     private String restaurantId;
+
+    @Override
+    public String toString() {
+        return "RestaurantDocument{" +
+                "id='" + id + '\'' +
+                ", restaurantName='" + restaurantName + '\'' +
+                ", restaurantId='" + restaurantId + '\'' +
+                '}';
+    }
 }
