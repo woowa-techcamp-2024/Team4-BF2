ffb70c27ec49341629283040437721d46621f322
huiseung <whtjddn75@gmail.com>
2024-08-09 20:32:56 +0900

    [feature] Restaurant에 관한 persistance 기능 - document, domain, mapper, repostiory 추가

---
+++ mono/src/main/java/woowa/team4/bff/search/repository/RestaurantEsRepository.java
@@ -0,0 +1,13 @@
+package woowa.team4.bff.search.repository;
+
+import java.util.List;
+import org.springframework.data.elasticsearch.annotations.Query;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import woowa.team4.bff.search.document.RestaurantDocument;
+
+public interface RestaurantEsRepository extends ElasticsearchRepository<RestaurantDocument, String> {
+    @Query("{\"match\": {\"restaurant_name\": {\"query\": \"?0\"}}}")
+    List<RestaurantDocument> findAllByRestaurantNameContaining(String restaurantName);
+
+    RestaurantDocument findByRestaurantId(String restaurantId);
+}
