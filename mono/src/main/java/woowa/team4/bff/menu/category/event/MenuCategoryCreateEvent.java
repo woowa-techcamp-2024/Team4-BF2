41793b5d7b8fe501c55f49607583c5dabdec9193
hellomatia <shuaigejp80@gmail.com>
2024-08-10 22:37:15 +0900

    [refactor] 같은 모듈내에서는 직접 호출하도록 하고, 생성된 정보 전부 이벤트안에 불변 객체로 관리하도록 변경
    - 같은 모듈내에서는 event를 발생시켜 처리하는 방식대신, 직접 처리하고 검색시스템에서 이벤트를 받아 동작하도록 변경한다.

--- mono/src/main/java/woowa/team4/bff/menu/category/event/MenuCategoryCreateEvent.java
+++ mono/src/main/java/woowa/team4/bff/menu/category/event/MenuCategoryCreateEvent.java
@@ -1,12 +1,31 @@
 package woowa.team4.bff.menu.category.event;
 
+import java.time.LocalDateTime;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import woowa.team4.bff.menu.category.entity.MenuCategory;
 
 @Getter
-@RequiredArgsConstructor
+@Builder
 public class MenuCategoryCreateEvent {
 
-    private final MenuCategory menuCategory;
+    private final Long id;
+    private final String uuid;
+    private final String restaurantUuid;
+    private final String name;
+    private final String description;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime updatedAt;
+
+    public static MenuCategoryCreateEvent from(final MenuCategory menuCategory) {
+        return MenuCategoryCreateEvent.builder()
+                .id(menuCategory.getId())
+                .uuid(menuCategory.getUuid())
+                .restaurantUuid(menuCategory.getRestaurantUuid())
+                .name(menuCategory.getName())
+                .description(menuCategory.getDescription())
+                .createdAt(menuCategory.getCreatedAt())
+                .updatedAt(menuCategory.getUpdatedAt())
+                .build();
+    }
 }
--- mono/src/main/java/woowa/team4/bff/menu/category/service/MenuCategoryService.java
+++ mono/src/main/java/woowa/team4/bff/menu/category/service/MenuCategoryService.java
@@ -4,23 +4,26 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
-import woowa.team4.bff.menu.category.dto.MenuCategoryDto;
+import woowa.team4.bff.menu.category.dto.MenuCategoryCreateDto;
 import woowa.team4.bff.menu.category.entity.MenuCategory;
 import woowa.team4.bff.menu.category.event.MenuCategoryCreateEvent;
+import woowa.team4.bff.menu.category.repository.MenuCategoryRepository;
 
 @Service
 @RequiredArgsConstructor
 public class MenuCategoryService {
 
     private final ApplicationEventPublisher eventPublisher;
+    private final MenuCategoryRepository menuCategoryRepository;
 
     @Transactional
-    public String createMenuCategory(MenuCategoryDto dto) {
+    public String createMenuCategory(MenuCategoryCreateDto dto) {
         MenuCategory menuCategory = new MenuCategory();
         menuCategory.setRestaurantUuid(dto.getRestaurantUuid());
         menuCategory.setName(dto.getName());
         menuCategory.setDescription(dto.getDescription());
-        eventPublisher.publishEvent(new MenuCategoryCreateEvent(menuCategory));
+        menuCategoryRepository.save(menuCategory);
+        eventPublisher.publishEvent(MenuCategoryCreateEvent.from(menuCategory));
         return menuCategory.getUuid();
     }
 }
