0c49f639b1f298deb007de8c9b7db0e99125605c
hellomatia <shuaigejp80@gmail.com>
2024-08-10 14:56:30 +0900

    [feature] 메뉴 카테고리의 데이터를 관리하는 repository 추가
    - 결합도를 줄이기 위해 repository 인터페이스로 분리한다.

---
+++ mono/src/main/java/woowa/team4/bff/menu/category/repository/JpaMenuCategoryRepository.java
@@ -0,0 +1,11 @@
+package woowa.team4.bff.menu.category.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+import woowa.team4.bff.menu.category.entity.MenuCategory;
+
+@Component
+public interface JpaMenuCategoryRepository extends JpaRepository<MenuCategory, Long> {
+
+    MenuCategory save(MenuCategory menuCategory);
+}
---
+++ mono/src/main/java/woowa/team4/bff/menu/category/repository/MenuCategoryRepository.java
@@ -0,0 +1,8 @@
+package woowa.team4.bff.menu.category.repository;
+
+import woowa.team4.bff.menu.category.entity.MenuCategory;
+
+public interface MenuCategoryRepository {
+    
+    MenuCategory save(MenuCategory menuCategory);
+}
