1ae45608d3f939e4fddd7984390dda7d6eb4f2c0
hellomatia <shuaigejp80@gmail.com>
2024-08-11 17:00:22 +0900

    [feature] PUT /menu/{menuUuid}, DELETE /menu/{menuUuid}로 요청이 들어올 시 메뉴를 수정, 삭제하는 기능 추가
    - PUT으로 요청이 들어오면 수정을 하고, DELETE로 요청이 들어오면 삭제를 한다.

--- mono/src/main/java/woowa/team4/bff/menu/item/controller/MenuController.java
+++ mono/src/main/java/woowa/team4/bff/menu/item/controller/MenuController.java
@@ -5,8 +5,10 @@ import static woowa.team4.bff.common.utils.ApiUtils.*;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -14,6 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 import woowa.team4.bff.menu.item.dto.create.MenuCreateDto;
 import woowa.team4.bff.menu.item.dto.create.MenuCreateRequest;
 import woowa.team4.bff.menu.item.dto.create.MenuCreateResponse;
+import woowa.team4.bff.menu.item.dto.update.MenuUpdateDto;
+import woowa.team4.bff.menu.item.dto.update.MenuUpdateRequest;
+import woowa.team4.bff.menu.item.dto.update.MenuUpdateResponse;
 import woowa.team4.bff.menu.item.service.MenuService;
 
 @RestController
@@ -30,5 +35,18 @@ public class MenuController {
         MenuCreateDto menuCreateDto = menuCreateRequest.toDto(menuCategoryUuid);
         String menuUuid = menuService.createMenu(menuCreateDto);
         return success(new MenuCreateResponse(menuUuid));
+    }
+
+    @PutMapping("/{menuUuid}")
+    public ApiResult<MenuUpdateResponse> updateMenu(@PathVariable final String menuUuid,
+            @Valid @RequestBody final MenuUpdateRequest MenuUpdateRequest) {
+        MenuUpdateDto menuUpdateDto = MenuUpdateRequest.toDto(menuUuid);
+        MenuUpdateDto updatedMenu = menuService.updateMenu(menuUpdateDto);
+        return success(MenuUpdateResponse.from(updatedMenu));
+    }
+
+    @DeleteMapping("/{menuUuid}")
+    public ApiResult<Boolean> deleteMenu(@PathVariable final String menuUuid) {
+        return success(menuService.deleteMenu(menuUuid));
     }
 }
