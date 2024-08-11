a9f5a6f23a09aa2474bf87386a8f9be9867f72d6
jcw1031 <jcw001031@gmail.com>
2024-08-11 20:54:31 +0900

    refactor: 가게 엔티티 수정
    - 매개변수를 가진 생성자와 빌더 추가
    - JPA auditing 적용을 위한 어노테이션 추가
    - Point 타입 변경
      - org.locationtech.jts.geom.Point로 변경

--- mono/src/main/java/woowa/team4/bff/restaurant/entity/RestaurantEntity.java
+++ mono/src/main/java/woowa/team4/bff/restaurant/entity/RestaurantEntity.java
@@ -5,9 +5,15 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
+import lombok.Builder;
 import lombok.Getter;
-import org.springframework.data.geo.Point;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.Point;
+import org.locationtech.jts.geom.PrecisionModel;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import woowa.team4.bff.restaurant.domain.Coordinate;
 
@@ -46,11 +52,37 @@ public class RestaurantEntity {
 
     private UUID uuid;
 
+    @CreatedDate
     private LocalDateTime createdAt;
 
+    @LastModifiedDate
     private LocalDateTime updatedAt;
 
+    public RestaurantEntity() {
+    }
+
+    @Builder
+    public RestaurantEntity(String name, String phone, String address, Coordinate location, String introduction, String image, String operatingTime, String closedDays, Integer minimumOrderAmount, Long businessId) {
+        this.name = name;
+        this.phone = phone;
+        this.address = address;
+        this.introduction = introduction;
+        this.image = image;
+        this.operatingTime = operatingTime;
+        this.closedDays = closedDays;
+        this.minimumOrderAmount = minimumOrderAmount;
+        this.businessId = businessId;
+
+        GeometryFactory geometryFactory = new GeometryFactory(new PrecisionModel(), 4326);
+        this.location = geometryFactory.createPoint(new org.locationtech.jts.geom.Coordinate(location.longitude(), location.latitude()));
+    }
+
+    @PrePersist
+    public void setupUuid() {
+        this.uuid = UUID.randomUUID();
+    }
+
     public Coordinate getLocation() {
-        return new Coordinate(location.getX(), location.getY());
+        return new Coordinate(location.getY(), location.getX());
     }
 }
