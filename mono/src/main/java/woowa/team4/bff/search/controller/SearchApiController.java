59bcd5a1a0cfcc164a5cdd2a008e539213ac488d
huiseung <whtjddn75@gmail.com>
2024-08-13 00:01:01 +0900

    [refactor] 모듈 이동 mono -> search-domain-rdb

--- mono/src/main/java/woowa/team4/bff/search/controller/SearchApiController.java
+++ mono/src/main/java/woowa/team4/bff/search/controller/SearchApiController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import woowa.team4.bff.common.utils.ApiUtils.ApiResult;
 import woowa.team4.bff.search.domain.RestaurantSearchResult;
-import woowa.team4.bff.search.service.SearchIndexManageService;
 import woowa.team4.bff.search.service.SearchService;
 import woowa.team4.bff.search.command.SearchRestaurantCommand;
 
@@ -19,7 +18,6 @@ import woowa.team4.bff.search.command.SearchRestaurantCommand;
 @RequiredArgsConstructor
 public class SearchApiController {
     private final SearchService searchService;
-    private final SearchIndexManageService searchIndexManageService;
 
     @GetMapping("")
     public ApiResult<List<RestaurantSearchResult>> searchRestaurants(
