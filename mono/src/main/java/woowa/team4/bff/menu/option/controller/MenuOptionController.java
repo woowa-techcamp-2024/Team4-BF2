22ae600fc08a5561234897c7a9fc5906360fa0d9
hellomatia <shuaigejp80@gmail.com>
2024-08-12 19:05:31 +0900

    [feature] PUT /menu/option/{menuOptionUuid}으로 요청을 보낼 시 메뉴 옵션을 수정하는 기능 추가

--- mono/src/main/java/woowa/team4/bff/menu/option/controller/MenuOptionController.java
+++ mono/src/main/java/woowa/team4/bff/menu/option/controller/MenuOptionController.java
@@ -5,14 +5,17 @@ import static woowa.team4.bff.common.utils.ApiUtils.*;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import woowa.team4.bff.menu.option.dto.create.MenuOptionCreateRequest;
 import woowa.team4.bff.menu.option.dto.create.MenuOptionCreateResponse;
+import woowa.team4.bff.menu.option.dto.update.MenuOptionUpdateRequest;
 import woowa.team4.bff.menu.option.service.MenuOptionService;
 
 @RestController
@@ -29,5 +32,13 @@ public class MenuOptionController {
             @Valid @RequestBody MenuOptionCreateRequest request) {
         String menuOptionUuid = menuOptionService.createMenuOption(request.toDto(menuUuid));
         return success(new MenuOptionCreateResponse(menuOptionUuid));
+    }
+
+    @PutMapping("/{menuOptionUuid}")
+    public ApiResult<Boolean> updateMenuOption(
+            @PathVariable String menuOptionUuid,
+            @RequestBody MenuOptionUpdateRequest request) {
+        menuOptionService.updateMenuOption(request.toDto(menuOptionUuid));
+        return success(Boolean.TRUE);
     }
 }
