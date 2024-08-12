c87b221ad22d822ad9637109c94007a3d04d5ea2
huiseung <whtjddn75@gmail.com>
2024-08-12 13:11:53 +0900

    [feature] RestaurantEntity 추가

--- mono/src/main/java/woowa/team4/bff/search/entity/RestaurantEntity.java
+++ mono/src/main/java/woowa/team4/bff/search/entity/RestaurantEntity.java
@@ -1,8 +1,15 @@
 package woowa.team4.bff.search.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
 
 @Entity
+@NoArgsConstructor
 public class RestaurantEntity {
+    @Id
+    @Generated
+    private Long id;
     // ToDo: Common 으로 빼는가, 중복 코드를 사용 하는가
 }
