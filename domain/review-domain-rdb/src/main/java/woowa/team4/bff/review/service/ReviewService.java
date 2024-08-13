7198eb81984aa0d7c29b499788ba31369362d8fe
hek-git <ghddmsrl1@naver.com>
2024-08-13 14:08:48 +0900

    [refactor] Review Service 클래스 createReview 메서드 변경 - service layer에서 command 클래스 -> domain 클래스로 변환 후 repository에 전달

---
+++ domain/review-domain-rdb/src/main/java/woowa/team4/bff/review/service/ReviewService.java
@@ -0,0 +1,27 @@
+package woowa.team4.bff.review.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import woowa.team4.bff.review.command.ReviewCreateCommand;
+import woowa.team4.bff.review.domain.Review;
+import woowa.team4.bff.review.repository.ReviewRepository;
+
+@Service
+@RequiredArgsConstructor
+public class ReviewService {
+
+    private final ReviewRepository reviewRepository;
+
+    @Transactional
+    public String createReview(final ReviewCreateCommand reviewCreateCommand) {
+
+        Review review = Review.builder()
+                .restaurantUuid(reviewCreateCommand.restaurantUuid())
+                .content(reviewCreateCommand.content())
+                .rating(reviewCreateCommand.rating())
+                .build();
+
+        return reviewRepository.save(review).getReviewUuId();
+    }
+}
