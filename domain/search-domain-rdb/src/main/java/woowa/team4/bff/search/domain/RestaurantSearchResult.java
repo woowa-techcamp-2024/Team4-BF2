b486f83325e1a3a7e91c02910d3774ebd019a9f6
huiseung <whtjddn75@gmail.com>
2024-08-13 18:10:14 +0900

    [feature] RestaurantSearchResult - 필요 테이블을 join 해서 가져오는 기능 추가

--- domain/search-domain-rdb/src/main/java/woowa/team4/bff/search/domain/RestaurantSearchResult.java
+++ domain/search-domain-rdb/src/main/java/woowa/team4/bff/search/domain/RestaurantSearchResult.java
@@ -1,11 +1,24 @@
 package woowa.team4.bff.search.domain;
 
+import java.util.UUID;
+import lombok.Getter;
+
+@Getter
 public class RestaurantSearchResult {
 
-    private String restaurantUuid;
+    private UUID restaurantUuid;
     private String restaurantName;
     private Integer minimumOrderPrice;
-    private Double rating;
-    private Integer reviewCount;
+    // ToDo: ReviewStatus Domain 기능 완성 후 추가
+//    private Double rating;
+//    private Integer reviewCount;
     private String menuName;
+
+    public RestaurantSearchResult(UUID restaurantUuid, String restaurantName, Integer minimumOrderPrice,
+                                  String menuName) {
+        this.restaurantUuid = restaurantUuid;
+        this.restaurantName = restaurantName;
+        this.minimumOrderPrice = minimumOrderPrice;
+        this.menuName = menuName;
+    }
 }
--- domain/search-domain-rdb/src/main/java/woowa/team4/bff/search/repository/RestaurantEntityRepository.java
+++ domain/search-domain-rdb/src/main/java/woowa/team4/bff/search/repository/RestaurantEntityRepository.java
@@ -9,11 +9,10 @@ import woowa.team4.bff.search.domain.RestaurantSearchResult;
 
 public interface RestaurantEntityRepository extends JpaRepository<RestaurantEntity, Long> {
     // ToDo: Restauarnt, Menu, ReviewStatus 관련 module 완성시 사용
-    @Query("SELECT new woowa.team4.bff.search.domain.RestaurantSearchResult(r.uuid, r.name, r.minimumOrderAmount, " +
-            "rs.rating, rs.reviewCount, m.menuName) " +
-            "FROM Restaurant r " +
-            "LEFT JOIN RestaurantReviewStatus rs ON r.id = rs.restaurantId " +
-            "LEFT JOIN Menu m ON r.id = m.restaurantId " +
+    @Query("SELECT new woowa.team4.bff.search.domain.RestaurantSearchResult(r.uuid, r.name, r.minimumOrderAmount, m.name) " +
+            "FROM RestaurantEntity r " +
+            "LEFT JOIN MenuCategoryEntity mc ON r.id = mc.restaurantId " +
+            "LEFT JOIN MenuEntity m ON mc.id = m.menuCategoryId " +
             "WHERE r.id IN :restaurantIds and r.deliveryLocation = :deliveryLocation")
     List<RestaurantSearchResult> findRestaurantSearchResults(@Param("restaurantIds") List<Long> restaurantIds, @Param("deliveryLocation") String deliveryLocation);
 }
