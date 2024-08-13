e115b24416ee2ba0ff112a350e8789367a416480
hek-git <ghddmsrl1@naver.com>
2024-08-13 12:28:14 +0900

    [refactor] ReviewCreateRequest 클래스 내 변환 메서드 제거 - 상위 계층 클래스인 Command 클래스로 변환하는 메서드 제거

--- mono/src/main/java/woowa/team4/bff/review/controller/create/ReviewCreateRequest.java
+++ mono/src/main/java/woowa/team4/bff/review/controller/create/ReviewCreateRequest.java
@@ -4,16 +4,8 @@ package woowa.team4.bff.review.controller.create;
 import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
-import woowa.team4.bff.review.command.ReviewCreateCommand;
 
 public record ReviewCreateRequest(@NotBlank String content,
                                   @DecimalMin(value = "0.0") @DecimalMax(value = "5.0") Double rating) {
 
-    public ReviewCreateCommand toCommand(final String restaurantUuid) {
-        return ReviewCreateCommand.builder()
-                .restaurantUuid(restaurantUuid)
-                .content(content)
-                .rating(rating)
-                .build();
-    }
 }
