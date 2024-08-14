3131ac39576f740e05b04e916b2179f3b06da824
jcw1031 <jcw001031@gmail.com>
2024-08-14 19:49:47 +0900

    [refactor] 가게등록•메뉴등록 이벤트 처리를 비동기로 실행하도록 변경
    - @Async 어노테이션을 사용해 비동기를 적용했습니다.
    - @EnableAsync 어노테이션을 추가해 비동기 사용을 활성화했습니다.

--- domain/search-domain-es/src/main/java/woowa/team4/bff/search/service/SearchIndexManageService.java
+++ domain/search-domain-es/src/main/java/woowa/team4/bff/search/service/SearchIndexManageService.java
@@ -3,6 +3,7 @@ package woowa.team4.bff.search.service;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -21,40 +22,44 @@ public class SearchIndexManageService {
     private final SearchIndexManageRepository searchIndexManageRepository;
     private final Logger log = LoggerFactory.getLogger(SearchIndexManageService.class);
 
+    @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-    public Long addRestaurant(RestaurantCreateEvent event) {
+    public void addRestaurant(RestaurantCreateEvent event) {
         RestaurantSearch restaurantSearch = RestaurantSearch.builder()
                 .restaurantId(event.restaurantId())
                 .restaurantName(event.restaurantName())
                 .build();
-        log.info("[addRestaurant]: "+restaurantSearch);
-        return searchIndexManageRepository.save(restaurantSearch);
+        log.info("[addRestaurant]: " + restaurantSearch);
+        searchIndexManageRepository.save(restaurantSearch);
     }
 
+    @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-    public Long updateRestaurant(RestaurantUpdateEvent event) {
+    public void updateRestaurant(RestaurantUpdateEvent event) {
         RestaurantSearch restaurantSearch = searchIndexManageRepository.findByRestaurantId(event.restaurantId());
         restaurantSearch.update(event.restaurantName());
-        log.info("[updateRestaurant]: "+restaurantSearch);
-        return searchIndexManageRepository.save(restaurantSearch);
+        log.info("[updateRestaurant]: " + restaurantSearch);
+        searchIndexManageRepository.save(restaurantSearch);
     }
 
+    @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-    public Long addMenu(MenuCreateEvent event) {
+    public void addMenu(MenuCreateEvent event) {
         MenuSearch menuSearch = MenuSearch.builder()
                 .menuId(event.menuId())
                 .menuName(event.menuName())
                 .restaurantId(event.restaurantId())
                 .build();
-        log.info("[addMenu]: "+menuSearch);
-        return searchIndexManageRepository.save(menuSearch);
+        log.info("[addMenu]: " + menuSearch);
+        searchIndexManageRepository.save(menuSearch);
     }
 
+    @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-    public Long updateMenu(MenuUpdateEvent event) {
+    public void updateMenu(MenuUpdateEvent event) {
         MenuSearch menuSearch = searchIndexManageRepository.findByMenuId(event.menuId());
         menuSearch.update(event.menuName());
-        log.info("[updateMenu]: "+menuSearch);
-        return searchIndexManageRepository.save(menuSearch);
+        log.info("[updateMenu]: " + menuSearch);
+        searchIndexManageRepository.save(menuSearch);
     }
 }
