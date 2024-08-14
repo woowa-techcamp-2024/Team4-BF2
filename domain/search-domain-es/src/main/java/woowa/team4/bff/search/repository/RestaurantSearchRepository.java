e71843a6ef9e003df40fff77cdcbbbd3306adeb0
jcw1031 <jcw001031@gmail.com>
2024-08-14 19:45:41 +0900

    [fix] Spring Data Elasticsearch 명시적 쿼리로 변경
    - 기존에는 메서드 네이밍으로 쿼리 생성했습니다.
    - @Query 어노테이션을 사용해 명시적으로 쿼리를 설정했습니다.

--- domain/search-domain-es/src/main/java/woowa/team4/bff/search/repository/RestaurantSearchRepository.java
+++ domain/search-domain-es/src/main/java/woowa/team4/bff/search/repository/RestaurantSearchRepository.java
@@ -1,13 +1,16 @@
 package woowa.team4.bff.search.repository;
 
 
-import java.util.List;
+import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import woowa.team4.bff.search.document.RestaurantSearchDocument;
+
+import java.util.List;
 
 public interface RestaurantSearchRepository extends ElasticsearchRepository<RestaurantSearchDocument, String> {
 
     // {"query": {"match": {"restaurantName": "검색어"}}}
+    @Query("{\"match\": {\"restaurantName\": \"?0\"}}")
     List<RestaurantSearchDocument> findByRestaurantNameContaining(String restaurantName);
 
     RestaurantSearchDocument findByRestaurantId(Long restaurantId);
