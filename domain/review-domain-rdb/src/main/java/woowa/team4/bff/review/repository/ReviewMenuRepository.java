09e61877ce8350bafb08462d18c70d4281643107
hek-git <ghddmsrl1@naver.com>
2024-08-15 15:11:12 +0900

    [feature] ReviewMenuEntity 저장하기 위한 ReviewMenuRepository 인터페이스 추가 - ReviewMenu 리스트를 저장하는 saveAll 메서드 추가 - Domain -> Entity로 변환하는 toEntity 메서드 추가 - Entity -> Domain로 변환하는 toDomain 메서드 추가

---
+++ domain/review-domain-rdb/src/main/java/woowa/team4/bff/review/repository/ReviewMenuRepository.java
@@ -0,0 +1,48 @@
+package woowa.team4.bff.review.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import woowa.team4.bff.domain.common.utils.PrefixedUuidConverter;
+import woowa.team4.bff.review.domain.ReviewMenu;
+import woowa.team4.bff.review.entity.ReviewMenuEntity;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ReviewMenuRepository {
+
+    private final ReviewMenuEntityRepository reviewMenuEntityRepository;
+
+    public List<ReviewMenu> saveAll(List<ReviewMenu> reviewMenu) {
+        List<ReviewMenuEntity> reviewMenuEntity = reviewMenu.stream()
+                .map(this::toEntity)
+                .toList();
+        reviewMenuEntityRepository.saveAll(reviewMenuEntity);
+
+        return reviewMenuEntity.stream()
+                .map(this::toDomain)
+                .toList();
+    }
+
+    private ReviewMenuEntity toEntity(ReviewMenu reviewMenu) {
+        return ReviewMenuEntity.builder()
+                .reviewId(reviewMenu.getReviewId())
+                .menuId(reviewMenu.getMenuId())
+                .content(reviewMenu.getContent())
+                .isLiked(reviewMenu.getIsLiked())
+                .build();
+    }
+
+    private ReviewMenu toDomain(ReviewMenuEntity reviewMenuEntity) {
+        return ReviewMenu.builder()
+                .reviewMenuUuId(PrefixedUuidConverter.addPrefix("review_menu", reviewMenuEntity.getUuid()))
+                .reviewId(reviewMenuEntity.getReviewId())
+                .menuId(reviewMenuEntity.getMenuId())
+                .content(reviewMenuEntity.getContent())
+                .isLiked(reviewMenuEntity.getIsLiked())
+                .build();
+    }
+
+
+}
