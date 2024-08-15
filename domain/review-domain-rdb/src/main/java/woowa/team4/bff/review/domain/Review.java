53b1c7b527e7a7ae687ba43d9679bafeed4130fe
hek-git <ghddmsrl1@naver.com>
2024-08-15 15:16:34 +0900

    [feature] Review 도메인 객체 필드 추가 - id 필드 추가 - restaurantUuid -> restaurantId로 변경

--- domain/review-domain-rdb/src/main/java/woowa/team4/bff/review/domain/Review.java
+++ domain/review-domain-rdb/src/main/java/woowa/team4/bff/review/domain/Review.java
@@ -2,13 +2,16 @@ package woowa.team4.bff.review.domain;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 
 @Builder
 @Getter
+@ToString
 public class Review {
 
+    private final Long id;
     private final String reviewUuId;
-    private final String restaurantUuid;
+    private final Long restaurantId;
     private final String content;
     private final Double rating;
 
