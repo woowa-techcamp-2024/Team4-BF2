d40695391af24d7beb1d4ec0691cb9965e093ba0
huiseung <whtjddn75@gmail.com>
2024-08-15 22:51:47 +0900

    [feature] mysql like

--- domain/restaurant-domain-rdb/src/main/java/woowa/team4/bff/restaurant/repository/RestaurantRepository.java
+++ domain/restaurant-domain-rdb/src/main/java/woowa/team4/bff/restaurant/repository/RestaurantRepository.java
@@ -1,5 +1,6 @@
 package woowa.team4.bff.restaurant.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import woowa.team4.bff.restaurant.entity.RestaurantEntity;
@@ -11,4 +12,5 @@ import java.util.UUID;
 public interface RestaurantRepository extends JpaRepository<RestaurantEntity, Long> {
 
     Optional<RestaurantEntity> findByUuid(UUID uuid);
+    List<RestaurantEntity> findByNameContaining(String restaurantName);
 }
