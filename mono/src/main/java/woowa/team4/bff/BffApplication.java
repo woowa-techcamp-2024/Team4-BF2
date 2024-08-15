9bb63d10882667e3090030c737e7bcd9bc77d0b0
jcw1031 <jcw001031@gmail.com>
2024-08-15 12:26:24 +0900

    [fix] gradle 멀티 모듈 의존성 정리 및 bootJar 옵션 수정
    - gradle 빌드 시 mainClass를 찾지 못해 오류가 발생했습니다.
    - runnable한 모듈인 mono 모듈만 bootJar.enable을 true로 설정했습니다.
    - 기존 중복되었던 의존성을 정리했습니다.

--- mono/src/main/java/woowa/team4/bff/BffApplication.java
+++ mono/src/main/java/woowa/team4/bff/BffApplication.java
@@ -2,12 +2,10 @@ package woowa.team4.bff;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
 @SpringBootApplication
-@EnableJpaAuditing
 public class BffApplication {
 
     public static void main(String[] args) {
