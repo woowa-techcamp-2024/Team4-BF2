8af9c3844c0872c10c439bfc41af93e2c18d1a08
hek-git <ghddmsrl1@naver.com>
2024-08-15 15:30:34 +0900

    [feature] ReviewService 클래스 변경 - ReviewCreateResult 객체 응답 - restaurantUuid가 아닌 id를 찾아 넣도록 변경 - menuUuid를 통해 menu의 id를 찾아 넣도록 함 - review 저장 이후 review의 pk를 포함시켜 reviewMenu 저장 - 반환된 id 값들을 가지고 Result 객체 생성 후 반환

--- domain/review-domain-rdb/src/main/java/woowa/team4/bff/review/service/ReviewService.java
+++ domain/review-domain-rdb/src/main/java/woowa/team4/bff/review/service/ReviewService.java
@@ -2,25 +2,58 @@ package woowa.team4.bff.review.service;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import woowa.team4.bff.menu.item.repository.MenuRepository;
+import woowa.team4.bff.restaurant.repository.RestaurantFinder;
 import woowa.team4.bff.review.command.ReviewCreateCommand;
 import woowa.team4.bff.review.domain.Review;
+import woowa.team4.bff.review.domain.ReviewMenu;
+import woowa.team4.bff.review.repository.ReviewMenuRepository;
 import woowa.team4.bff.review.repository.ReviewRepository;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ReviewService {
 
     private final ReviewRepository reviewRepository;
+    private final ReviewMenuRepository reviewMenuRepository;
+    private final RestaurantFinder restaurantFinder;
+    private final MenuRepository menuRepository;
 
     @Transactional
-    public String createReview(final ReviewCreateCommand reviewCreateCommand) {
+    public ReviewCreateResult createReview(final ReviewCreateCommand reviewCreateCommand) {
+
+        // Uuid를 이용하여 restaurantId(PK)를 찾고, review Entity를 먼저 저장한다.
+        Long restaurantId = restaurantFinder.findIdByUuid(reviewCreateCommand.restaurantUuid());
         Review review = Review.builder()
-                .restaurantUuid(reviewCreateCommand.restaurantUuid())
+                .restaurantId(restaurantId)
                 .content(reviewCreateCommand.content())
                 .rating(reviewCreateCommand.rating())
                 .build();
+        Review savedReview = reviewRepository.save(review);
 
-        return reviewRepository.save(review).getReviewUuId();
+        // 저장된 review Entity의 pk를 갖고 reviewMenu를 추가한다.
+        List<ReviewMenu> reviewMenus = reviewCreateCommand.menus().stream()
+                .map(menu -> ReviewMenu.builder()
+                        .reviewId(savedReview.getId())
+                        .menuId(menuRepository.findByUuid(menu.menuUuid()).getId())
+                        .content(menu.content())
+                        .isLiked(menu.isRecommend())
+                        .build())
+                .toList();
+        List<ReviewMenu> savedReviewMenus = reviewMenuRepository.saveAll(reviewMenus);
+
+        log.info("[ReviewMenus] : {}", savedReviewMenus);
+        log.info("[Review] : {}", savedReview.toString());
+
+        return ReviewCreateResult.builder()
+                .reviewUuid(savedReview.getReviewUuId())
+                .reviewMenuUuids(savedReviewMenus.stream()
+                        .map(ReviewMenu::getReviewMenuUuId).toList())
+                .build();
     }
 }
