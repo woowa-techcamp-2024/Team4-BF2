21e298942b6117731a407f36cf7621232b26b7b6
huiseung <whtjddn75@gmail.com>
2024-08-16 16:25:14 +0900

    [feature] 다양한 검색 쿼리 추가

--- mono/src/main/java/woowa/team4/bff/search/service/SearchService.java
+++ mono/src/main/java/woowa/team4/bff/search/service/SearchService.java
@@ -13,40 +13,47 @@ import woowa.team4.bff.search.domain.MenuSearch;
 import woowa.team4.bff.search.domain.RestaurantSearch;
 import woowa.team4.bff.search.domain.RestaurantSearchResult;
 import woowa.team4.bff.search.repository.RestaurantSearchResultRepository;
-import woowa.team4.bff.search.repository.SearchRepository;
+import woowa.team4.bff.search.repository.SearchEsRepository;
 
 @Service
 @RequiredArgsConstructor
 public class SearchService {
-    private final SearchRepository searchRepository;
+    private final SearchEsRepository searchEsRepository;
     private final RestaurantFinder restaurantFinder;
     private final RestaurantSearchResultRepository restaurantSearchResultRepository;
     private final Logger log = LoggerFactory.getLogger(SearchService.class);
 
     public List<RestaurantSearchResult> searchEs(SearchRestaurantCommand command) {
-        return restaurantSearchResultRepository.findByRestaurantIdsAndDeliveryLocation(getRestaurantIds(command.keyword()), command.deliveryLocation());
+        return restaurantSearchResultRepository.findByRestaurantIdsAndDeliveryLocation(getRestaurantIdsInEs(command.keyword()), command.deliveryLocation());
     }
 
-    public List<RestaurantSearchResult> searchLike(SearchRestaurantCommand command) {
+    private List<Long> getRestaurantIdsInEs(String keyword) {
         long start = System.currentTimeMillis();
-        List<Long> ids = restaurantFinder.findIdByKeyword(command.keyword());
-        log.info("Method 'findIdByKeyword' execution time: " + (System.currentTimeMillis() - start));
-        log.info("hit-restaurantIds-by-restaurantName "+ ids);
-        return restaurantSearchResultRepository.findByRestaurantIdsAndDeliveryLocation(ids, command.deliveryLocation());
-    }
-
-    public List<Long> getRestaurantIds(String keyword) {
-        long start = System.currentTimeMillis();
-        List<RestaurantSearch> restaurantSearches = searchRepository.findAllByRestaurantName(keyword);
-        // List<MenuSearch> menuSearches = searchRepository.findAllByMenuName(keyword);
+        List<RestaurantSearch> restaurantSearches = searchEsRepository.findAllByRestaurantName(keyword);
+        List<MenuSearch> menuSearches = searchEsRepository.findAllByMenuName(keyword);
         log.info("Method 'getRestaurantIds' execution time: " + (System.currentTimeMillis() - start));
         List<Long> ids = new ArrayList<>();
         List<Long> results = restaurantSearches.stream().map(RestaurantSearch::getRestaurantId).toList();
         ids.addAll(results);
         log.info("hit-restaurantIds-by-restaurantName "+ ids);
-        // results = menuSearches.stream().map(MenuSearch::getRestaurantId).toList();
-        // log.info("hit-restaurantIds-by-menuName");
-        // ids.addAll(results);
+        results = menuSearches.stream().map(MenuSearch::getRestaurantId).toList();
+        log.info("hit-restaurantIds-by-menuName");
+        ids.addAll(results);
         return ids;
+    }
+
+
+    public List<RestaurantSearchResult> searchLikeAndJoin(SearchRestaurantCommand command) {
+        return restaurantSearchResultRepository.findByRestaurantNameAndDeliveryLocation(command.keyword(), command.deliveryLocation());
+    }
+
+    public List<RestaurantSearchResult> searchLikeAfterJoin(SearchRestaurantCommand command) {
+        long start = System.currentTimeMillis();
+        List<Long> ret = new ArrayList<>();
+        List<Long> ids = restaurantSearchResultRepository.findIdsByKeywordAndDeliveryLocation(command.keyword(), command.deliveryLocation());
+        log.info("Method 'findIdsByKeywordAndDeliveryLocation' execution time: " + (System.currentTimeMillis() - start));
+        log.info("hit-restaurantIds-by-restaurantName "+ ids);
+        ret.addAll(ids);
+        return restaurantSearchResultRepository.findByRestaurantIds(ret);
     }
 }
