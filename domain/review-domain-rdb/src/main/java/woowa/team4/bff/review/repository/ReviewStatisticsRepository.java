2ae02331ccb9997ba495ca440f05842c078493bc
hek-git <ghddmsrl1@naver.com>
2024-08-16 15:57:16 +0900

    [feature] : ReviewStatistics 도메인 객체를 처리할 수 있는 ReviewStaticsRepository 클래스 추가 - 식당 Id를 이용하여 해당 식당의 리뷰 통계를 조회하는 findByRestaurantId 메서드 구현 - Entity를 저장하는 save 메서드 구현 - Domain -> Entity 변환 메서드 구현 - Entity -> Domain 변환 메서드 구현

---
+++ domain/review-domain-rdb/src/main/java/woowa/team4/bff/review/repository/ReviewStatisticsRepository.java
@@ -0,0 +1,44 @@
+package woowa.team4.bff.review.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import woowa.team4.bff.review.domain.ReviewStatistics;
+import woowa.team4.bff.review.entity.ReviewStatisticsEntity;
+
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class ReviewStatisticsRepository {
+
+    private final ReviewStatisticsEntityRepository reviewStatisticsEntityRepository;
+
+    public Optional<ReviewStatistics> findByRestaurantId(Long restaurantId) {
+        Optional<ReviewStatisticsEntity> reviewStatisticsEntity = reviewStatisticsEntityRepository.findByRestaurantId(restaurantId);
+        return reviewStatisticsEntity.map(this::toDomain);
+    }
+
+    public ReviewStatistics save(ReviewStatistics reviewStatistics) {
+        ReviewStatisticsEntity save = reviewStatisticsEntityRepository.save(toEntity(reviewStatistics));
+        return toDomain(save);
+    }
+
+    public ReviewStatistics toDomain(ReviewStatisticsEntity reviewStatisticsEntity) {
+        return ReviewStatistics.builder()
+                .id(reviewStatisticsEntity.getId())
+                .restaurantId(reviewStatisticsEntity.getRestaurantId())
+                .reviewCount(reviewStatisticsEntity.getReviewCount())
+                .averageRating(reviewStatisticsEntity.getAverageRating())
+                .build();
+    }
+
+    public ReviewStatisticsEntity toEntity(ReviewStatistics reviewStatistics) {
+        return ReviewStatisticsEntity.builder()
+                .id(reviewStatistics.getId())
+                .restaurantId(reviewStatistics.getRestaurantId())
+                .reviewCount(reviewStatistics.getReviewCount())
+                .averageRating(reviewStatistics.getAverageRating())
+                .build();
+    }
+
+}
