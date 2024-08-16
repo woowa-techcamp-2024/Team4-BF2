bdd160289864a098d4f336c99329a9d22672fc44
huiseung <whtjddn75@gmail.com>
2024-08-16 16:25:33 +0900

    [refactor] 클래스 이름 변경, 쓰지 않는 메서드 제거

--- domain/restaurant-domain-rdb/src/main/java/woowa/team4/bff/restaurant/repository/RestaurantRepository.java
+++ domain/restaurant-domain-rdb/src/main/java/woowa/team4/bff/restaurant/repository/RestaurantRepository.java
@@ -1,6 +1,5 @@
 package woowa.team4.bff.restaurant.repository;
 
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import woowa.team4.bff.restaurant.entity.RestaurantEntity;
@@ -12,5 +11,4 @@ import java.util.UUID;
 public interface RestaurantRepository extends JpaRepository<RestaurantEntity, Long> {
 
     Optional<RestaurantEntity> findByUuid(UUID uuid);
-    List<RestaurantEntity> findByNameContaining(String restaurantName);
 }
