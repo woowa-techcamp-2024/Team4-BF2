713c7844260a9e5a3c18a1e02cdd6c50749efbe6
jcw1031 <jcw001031@gmail.com>
2024-08-20 16:56:44 +0900

    [feature] 가게•리뷰 통계 캐시 서비스 구현
    - 가게 ID 목록이 전달되면 Redis에서 조회해 반환
    - 만약 Redis에 존재하지 않으면 RDB에서 조회해 Redis에 저장한 후 반환
    - 리뷰 통계 변경 이벤트가 발생하면 Redis에 이미 저장된 가게의 리뷰 통계 정보라면 변경

--- domain/cache-domain-redis/src/main/java/woowa/team4/bff/cache/redis/repository/RestaurantReviewStatisticsRepository.java
+++ domain/cache-domain-redis/src/main/java/woowa/team4/bff/cache/redis/repository/RestaurantReviewStatisticsRepository.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
-public class RestaurantSummaryRepository {
+public class RestaurantReviewStatisticsRepository {
 
     private final RestaurantRepository restaurantRepository;
     private final ReviewStatisticsEntityRepository reviewStatisticsEntityRepository;
