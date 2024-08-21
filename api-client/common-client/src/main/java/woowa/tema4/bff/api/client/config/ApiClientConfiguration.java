c31b3a61cdeb63272e211623feee57e379738a15
hellomatia <shuaigejp80@gmail.com>
2024-08-22 01:29:51 +0900

    [config] 비동기 요청에 필요한 WebClientCaller를 빈으로 등록

--- api-client/common-client/src/main/java/woowa/tema4/bff/api/client/config/ApiClientConfiguration.java
+++ api-client/common-client/src/main/java/woowa/tema4/bff/api/client/config/ApiClientConfiguration.java
@@ -3,10 +3,17 @@ package woowa.tema4.bff.api.client.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 import woowa.tema4.bff.api.client.caller.ExternalApiCaller;
+import woowa.tema4.bff.api.client.caller.WebClientCaller;
 
 @Configuration
 public class ApiClientConfiguration {
+
+    @Bean
+    public ApiClientProperties apiClientProperties() {
+        return new ApiClientProperties();
+    }
 
     @Bean
     public RestTemplate restTemplate() {
@@ -19,7 +26,12 @@ public class ApiClientConfiguration {
     }
 
     @Bean
-    public ApiClientProperties apiClientProperties() {
-        return new ApiClientProperties();
+    public WebClient webClient() {
+        return WebClient.builder().build();
+    }
+
+    @Bean
+    public WebClientCaller webClientCaller(WebClient webClient) {
+        return new WebClientCaller(webClient);
     }
 }
