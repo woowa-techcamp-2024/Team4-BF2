a203d7a4cfea4c4bee8f6744f43b15c63ed9fb7c
huiseung <whtjddn75@gmail.com>
2024-08-22 21:30:37 +0900

    [feature] 서킷 브레이커 추가

--- api-client/common-client/src/main/java/woowa/tema4/bff/api/client/caller/WebClientCaller.java
+++ api-client/common-client/src/main/java/woowa/tema4/bff/api/client/caller/WebClientCaller.java
@@ -1,17 +1,22 @@
 package woowa.tema4.bff.api.client.caller;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.reactor.circuitbreaker.operator.CircuitBreakerOperator;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+@Slf4j
+@RequiredArgsConstructor
 public class WebClientCaller {
 
     private final WebClient webClient;
+    private final CircuitBreaker circuitBreaker;
 
-    public WebClientCaller(WebClient webClient) {
-        this.webClient = webClient;
-    }
 
     public <T, R> Mono<R> post(String url, T requestBody,
             ParameterizedTypeReference<R> responseType) {
@@ -20,7 +25,15 @@ public class WebClientCaller {
                 .contentType(MediaType.APPLICATION_JSON)
                 .bodyValue(requestBody)
                 .retrieve()
-                .bodyToMono(responseType);
+                .bodyToMono(responseType)
+                .transform(CircuitBreakerOperator.of(circuitBreaker))
+                .onErrorResume(throwable -> fallback(throwable))
+                ;
+    }
+
+    private Mono fallback(Throwable t) {
+        log.error("error fallback");
+        return Mono.empty();
     }
 
     // 블로킹 방식으로 결과를 받고 싶은 경우 사용할 수 있는 메서드
