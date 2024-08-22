a44ba3e53405297d462c58225561b844bcfaef0b
huiseung <whtjddn75@gmail.com>
2024-08-22 21:15:18 +0900

    [refactor] 외부 api 서버 분리

---
+++ external/advertisement-external/src/test/java/woowa/team4/advertisementexternal/AdvertisementExternalApplicationTests.java
@@ -0,0 +1,13 @@
+package woowa.team4.advertisementexternal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvertisementExternalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
