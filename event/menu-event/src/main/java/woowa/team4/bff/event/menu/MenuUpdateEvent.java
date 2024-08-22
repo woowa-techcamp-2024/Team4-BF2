33893fd8cdcbb9999c1a40eeba60c3feda570b1d
jcw1031 <jcw001031@gmail.com>
2024-08-22 21:50:57 +0900

    [refactor] 이벤트 객체에 필드 추가
    - 가게 등록 이벤트 객체에 배달 위치 필드 추가
    - 메뉴 수정 이벤트 객체에 가게 ID 필드 추가

--- event/menu-event/src/main/java/woowa/team4/bff/event/menu/MenuUpdateEvent.java
+++ event/menu-event/src/main/java/woowa/team4/bff/event/menu/MenuUpdateEvent.java
@@ -2,6 +2,6 @@ package woowa.team4.bff.event.menu;
 
 import woowa.team4.bff.event.Event;
 
-public record MenuUpdateEvent(Long menuId, String menuName) implements Event {
+public record MenuUpdateEvent(Long menuId, String menuName, Long restaurantId) implements Event {
 
 }
