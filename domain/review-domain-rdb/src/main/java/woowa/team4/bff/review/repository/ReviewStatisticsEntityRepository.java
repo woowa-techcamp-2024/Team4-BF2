9dc8e956907101c8fa89c1b92b90cbcb47f79fb0
hek-git <ghddmsrl1@naver.com>
2024-08-22 20:23:12 +0900

    [fix] 동시성 이슈 해결을 위한 메서드 추가 - update를 위해 비관적 락을 사용하는 findByRestaurantIdUsingPessimisticLock 메서드 추가

--- domain/review-domain-rdb/src/main/java/woowa/team4/bff/review/repository/ReviewStatisticsEntityRepository.java
+++ domain/review-domain-rdb/src/main/java/woowa/team4/bff/review/repository/ReviewStatisticsEntityRepository.java
@@ -1,6 +1,9 @@
 package woowa.team4.bff.review.repository;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 import woowa.team4.bff.review.entity.ReviewStatisticsEntity;
 
 import java.util.Optional;
@@ -8,5 +11,9 @@ import java.util.Optional;
 public interface ReviewStatisticsEntityRepository extends JpaRepository<ReviewStatisticsEntity, Long> {
 
     Optional<ReviewStatisticsEntity> findByRestaurantId(Long restaurantId);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT r FROM ReviewStatisticsEntity r WHERE r.restaurantId = :restaurantId")
+    Optional<ReviewStatisticsEntity> findByRestaurantIdUsingPessimisticLock(Long restaurantId);
 
 }
