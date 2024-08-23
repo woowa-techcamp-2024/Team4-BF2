b5164816114e83d46b2fe18c3df2f3153d993c96
huiseung <whtjddn75@gmail.com>
2024-08-23 16:01:00 +0900

    [refactor] 서버 분리 - 모듈 생성 - SpringApplication 최초 생성

---
+++ service/cache-service/src/test/java/woowa/team4/cacheservice/CacheServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package woowa.team4.cacheservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ service/restaurant-service/src/main/java/woowa/team4/bff/RestaurantServiceApplication.java
@@ -0,0 +1,13 @@
+package woowa.team4.bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantServiceApplication.class, args);
+    }
+
+}
