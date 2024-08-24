c92def013f7809eb4e76b293f3d2a4d0e1af2979
hellomatia <shuaigejp80@gmail.com>
2024-08-25 00:20:26 +0900

    [fix] 등록이 안되는 현상 해결
    - 등록과 조회에 맞는 경로를 설정해준다.

--- view/vite.config.js
+++ view/vite.config.js
@@ -1,14 +1,25 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:8080/api',
+      '/api/v1/restaurant-summary': {
+        target: 'http://localhost:8080/api/v1/restaurant-summary',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+        rewrite: (path) => path.replace(/^\/api\/v1\/restaurant-summary/, '')
+      },
+      '/api/v1/restaurants': {
+        target: 'http://localhost:7002/api/v1/restaurants',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/v1\/restaurants/,
+            '')
+      },
+      '/api/v1/menus': {
+        target: 'http://localhost:7002/api/v1/menus',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/v1\/menus/, '')
       }
     }
   }
