15183ec9d8afac642e29e85ce6499751f1f3f349
huiseung <whtjddn75@gmail.com>
2024-08-25 23:57:57 +0900

    [feature] redis multi get / bulk insert 추가 - es에서 검색시 id를 최대 250 가져오기에 다량에 데이터를 한번에 가져오거나, 한번에 넣는 코드 추가 - miss시 추가하는 로직을 async로 해 redis 통신을 대기 하지 않고 우선 exposure-service에게 응답할 수 있게 수정

--- domain/cache-domain-redis/src/main/java/woowa/team4/bff/cache/redis/service/CacheManagerService.java
+++ domain/cache-domain-redis/src/main/java/woowa/team4/bff/cache/redis/service/CacheManagerService.java
@@ -1,21 +1,27 @@
 package woowa.team4.bff.cache.redis.service;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SessionCallback;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
+import woowa.team4.bff.cache.redis.repository.RestaurantReviewStatisticsRepository;
 import woowa.team4.bff.cache.redis.utils.JsonConverter;
 import woowa.team4.bff.cache.redis.utils.RedisKeyMaker;
 import woowa.team4.bff.domain.RestaurantSummary;
 import woowa.team4.bff.event.cache.DeliveryLocationAndKeywordCreateEvent;
+import woowa.team4.bff.event.cache.RestaurantSummaryCacheEvent;
 import woowa.team4.bff.event.reviewstatistics.ReviewStatisticsUpdateEvent;
+import woowa.team4.bff.search.repository.RestaurantSummaryRepository;
 
 @Service
 @RequiredArgsConstructor
@@ -23,6 +29,8 @@ public class CacheManagerService {
 
     private final RedisTemplate<String, String> redisTemplate;
     private final JsonConverter jsonConverter;
+    private final RestaurantSummaryRepository restaurantSummaryRepository;
+
 
     @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
@@ -50,5 +58,30 @@ public class CacheManagerService {
                 1,
                 TimeUnit.HOURS
         );
+    }
+
+    @Async
+    @EventListener
+    public void handle(RestaurantSummaryCacheEvent event){
+        List<RestaurantSummary> restaurantSummaries = restaurantSummaryRepository.findByRestaurantIds(event.restaurantIds());
+        bulkPutRestaurantSummaries(restaurantSummaries);
+    }
+
+    // redis 네트워크 통신 비용 때문에 한번의 통신에 여러 insert 보냄
+    private void bulkPutRestaurantSummaries(List<RestaurantSummary> restaurantSummaries) {
+        redisTemplate.execute(new SessionCallback<List<Object>>() {
+            @Override
+            public List<Object> execute(RedisOperations operations) throws DataAccessException {
+                // 트랜잭션 시작
+                operations.multi();
+
+                for (RestaurantSummary summary : restaurantSummaries) {
+                    String key = RedisKeyMaker.makeRestaurantKey(summary.getId());
+                    operations.opsForValue().set(key, jsonConverter.convert(summary));
+                }
+                // 명령어 전송
+                return operations.exec();
+            }
+        });
     }
 }
