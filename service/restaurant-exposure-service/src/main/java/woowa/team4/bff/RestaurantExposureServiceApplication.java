131203245eaef00d46b4c8c44ef6abe80099d9c9
hellomatia <shuaigejp80@gmail.com>
2024-08-28 16:25:49 +0900

    [feature] 요청과 응답을 비동기로 실행시키도록 변경
    - WebFlux Netty로 실행하도록 하여 요청과 응답을 비동기 형태로 받도록 한다.

--- service/restaurant-exposure-service/src/main/java/woowa/team4/bff/RestaurantExposureServiceApplication.java
+++ service/restaurant-exposure-service/src/main/java/woowa/team4/bff/RestaurantExposureServiceApplication.java
@@ -2,8 +2,10 @@ package woowa.team4.bff;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
+@EnableWebFlux
 public class RestaurantExposureServiceApplication {
 
     public static void main(String[] args) {
--- service/restaurant-exposure-service/src/main/java/woowa/team4/bff/restauarntexposureservice/exposure/controller/RestaurantSummaryApiController.java
+++ service/restaurant-exposure-service/src/main/java/woowa/team4/bff/restauarntexposureservice/exposure/controller/RestaurantSummaryApiController.java
@@ -1,18 +1,18 @@
 package woowa.team4.bff.restauarntexposureservice.exposure.controller;
 
 
-import static woowa.team4.bff.common.utils.ApiUtils.success;
-
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import woowa.team4.bff.common.utils.ApiUtils.ApiResult;
+import reactor.core.publisher.Mono;
 import woowa.team4.bff.domain.ExposureRestaurantSummary;
 import woowa.team4.bff.exposure.command.SearchCommand;
 import woowa.team4.bff.exposure.service.RestaurantExposureListService;
+import woowa.team4.bff.restauarntexposureservice.exposure.utils.ApiUtils;
+import woowa.team4.bff.restauarntexposureservice.exposure.utils.ApiUtils.ApiResult;
 
 @RequestMapping("/api/v1/restaurant-summary")
 @RestController
@@ -22,13 +22,13 @@ public class RestaurantSummaryApiController {
     private final RestaurantExposureListService restaurantExposureListService;
 
     @GetMapping("/cache")
-    public ApiResult<List<ExposureRestaurantSummary>> getRestaurantSummaries(
+    public Mono<ApiResult<List<ExposureRestaurantSummary>>> getRestaurantSummaries(
             @RequestParam("keyword") String keyword,
             @RequestParam("deliveryLocation") String deliveryLocation,
             @RequestParam("pageNumber") Integer pageNumber
     ) {
-        List<ExposureRestaurantSummary> response = restaurantExposureListService.search(
-                new SearchCommand(keyword, deliveryLocation, pageNumber));
-        return success(response);
+        return restaurantExposureListService.search(
+                        new SearchCommand(keyword, deliveryLocation, pageNumber))
+                .map(ApiUtils::success);
     }
 }
